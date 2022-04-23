05bb5d453a5b98c9e1cfaf4fb08dc8baa9152f81
Aura Russill <auracris1996@hotmail.com>
2022-04-23 15:36:31 -0500

    ADD & DELETE - addterminarJuego deleteEstados.json

--- app.js
+++ app.js
@@ -1,5 +1,36 @@
-import { Respuesta } from'./lib/entidades/respuesta.js'
+import { Respuesta } from "./lib/entidades/respuesta.js";
+import { Juego } from "./lib/entidades/juego.js";
 
-var respuesta = new Respuesta()
-respuesta.mostrarRespuestas(3)
-respuesta.validarRespuesta()
+let juego1 = {
+  id: 1,
+  puntaje: 0,
+  ronda_actual: 1,
+  estado: "activo",
+  jugador: {
+    id: 123,
+    nombre: "Sergio",
+    edad: 24,
+  },
+};
+
+let respuesta = new Respuesta();
+respuesta.mostrarRespuestas(3);
+
+let juego = new Juego(
+  juego1.id,
+  juego1.puntaje,
+  juego1.ronda_actual,
+  juego1.jugador,
+  juego1.estado
+);
+
+const terminar = document.getElementById("terminar");
+terminar.addEventListener("click", () => {
+  juego.terminarJuego();
+});
+
+
+const nuevoJuego = document.querySelector("#btnNuevoJuego");
+nuevoJuego.addEventListener("click", () =>{
+    jugo.nuevoJuego()
+})
