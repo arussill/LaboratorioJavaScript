d834e95ee179f95537fbee44e2a76d9e11cae453
garciar316 <sergio980316@hotmail.com>
2022-04-24 11:37:31 -0500

    Co-authored-by: Santiago A. Viana V. <salviega@users.noreply.github.com>

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ import { Juego } from './lib/entidades/juego.js'
 
 let juego = new Juego()
 
-document.querySelector('#inicio').addEventListener('click', () => juego.iniciarJuego())
+document.querySelector('#inicio').addEventListener('click', () => juego.crearJuego())
 document.querySelector('#historial').addEventListener('click', () => juego.leerJuegos())
